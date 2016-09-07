47a7a0410fb6d36b12a3db7dbe3aaf1eded17213
Valery Yundin <yuvalery@gmail.com>
2016-09-07 23:24:42 +0200

    fix js

--- javascripts/all.js
+++ javascripts/all.js
@@ -141,7 +141,8 @@ function markStrikethrough() {
   var stuff = [];
   $('section').children().each(function() {
     if (head !== null) {
-      if ($(this).prop('tagName') >= head.prop('tagName')) {
+      if ($(this).prop('tagName')[0] == head.prop('tagName')[0] &&
+          $(this).prop('tagName') >= head.prop('tagName')) {
         stuff = $(stuff).map(function () { return this.toArray(); });
         (function(head, stuff) {
           head.css('background-color', '#FFDDDD');
